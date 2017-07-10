a255ccbcbe42fcd05da056f9c42196cffbfa8e48
aczhengliu <aczhengliu@tencent.com>
2017-07-10 11:25:09 +0800

    去掉多余的代码

--- platforms/android/src/org/apache/cordova/x5engine/CordovaX5WebViewActivity.java
+++ platforms/android/src/org/apache/cordova/x5engine/CordovaX5WebViewActivity.java
@@ -1,18 +1,14 @@
 package org.apache.cordova.x5engine;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
 import com.tencent.smtt.sdk.QbSdk;
-import com.tencent.smtt.sdk.WebView;
 
 import org.apache.cordova.CordovaActivity;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaWebViewImpl;
 
-import io.cordova.hellocordova.R;
 
 
 /**
@@ -21,24 +17,25 @@ import io.cordova.hellocordova.R;
 
 public class CordovaX5WebViewActivity extends CordovaActivity {
 
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.init();
+        //自定义native页面布局　step 1
 //        setContentView(R.layout.activity_test_cordova_with_layout);
 //        ((TextView)findViewById(R.id.tvTip)).setText("X5CoreVersion:" + QbSdk.getTbsVersion(CordovaX5WebViewActivity.this));
-        loadUrl("http://www.baidu.com");
         Log.i(TAG, "CordovaX5WebViewActivity: x5CoreVersion:" + QbSdk.getTbsVersion(CordovaX5WebViewActivity.this));
     }
 
     @Override
     protected CordovaWebView makeWebView() {
+        //自定义native页面布局　step 2
 //      X5WebView webView = (X5WebView)findViewById(R.id.cordovaWebView);
         X5WebView webView = new X5WebView(CordovaX5WebViewActivity.this);
         return new CordovaWebViewImpl(new X5WebViewEngine(webView));
     }
 
 }
+
 
